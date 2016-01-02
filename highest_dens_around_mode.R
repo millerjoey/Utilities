22e0f96cb8dc500e982a3b80c23867fd9106c025
millerjoey <arch1190@gmail.com>
2016-01-02 10:55:27 -0500

    Update highest_dens_around_mode.R

--- highest_dens_around_mode.R
+++ highest_dens_around_mode.R
@@ -1,26 +1,34 @@
-# Computes highest size P interval about mode of (normalized or
-# unnormalized) distribution, returns vector with the indices 
-# and prints indices with contained probability.
+# Computes highest size P interval about mode (default) of 
+# (normalized or unnormalized) distribution, returns vector 
+# with the indices and prints indices with contained probability.
+# change "start= " to include 
 # ----------------------------------------------------------------
 
-interval.around.mode1D <- function(density, p, mode=max(normalized_data)) {
+interval.around.mode1D <- function(density, p, use.mode=True) {
   sum <- 0
   i <- 1
   k <- 1
-  mode <- max(density)
-  modeVector <- mode == density
-  modeIndex <- (1:length(density))[modeVector]
   
-  if (length(modeIndex)>1) {print("Two Modes exist; choosing one", quote = F) ; modeIndex <- modeIndex[1]}
+  if (use.mode==TRUE) {
+    mode <- max(density)
+    indexVector <- mode == density
+  }
+  if (use.mode==FALSE) {
+  
+  }
+  
+  index <- (1:length(density))[indexVector]
+      
+  if (length(index)>1) {print("Two Modes exist; choosing one", quote = F) ; index <- index[1]}
 
   density <- density/sum(density)
   
   while (sum < p) { # This starts at mode and expands left or right, depending on which has more mass
-    sum <- sum(density[(modeIndex-i):(modeIndex+k)])
-    if (density[modeIndex-i] <= density[modeIndex+k]) {k <- k + 1}
+    sum <- sum(density[(index-i):(index+k)])
+    if (density[index-i] <= density[index+k]) {k <- k + 1}
     else {i <- i + 1} 
   }
-  print(c("Indices of interval:", modeIndex-i, modeIndex+k), quote = F)
+  print(c("Indices of interval:", index-i, index+k), quote = F)
   print(c("Contains the following probability:", sum), quote = F)
-  return(c(modeIndex-i, modeIndex+k))
+  return(c(index-i, index+k))
 }
